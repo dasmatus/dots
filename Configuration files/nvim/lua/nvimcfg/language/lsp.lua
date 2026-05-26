582d3ed825dd3b0f9707974f2a002079d169ac99
Matus Mastena <Shadiness9530@proton.me>
2026-05-26 10:02:46 +0200

    refactor: extract treesitter config into dedicated module

--- Configuration files/nvim/lua/nvimcfg/language/lsp.lua
+++ Configuration files/nvim/lua/nvimcfg/language/lsp.lua
@@ -1,16 +1,3 @@
 require("trouble").setup()
 require("todo-comments").setup()
 require("barbecue.ui").toggle(true)
-require('nvim-treesitter.configs').setup {
-	auto_install = true,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting=false,
-	},
-	ident = { enable = true },
-	rainbow = {
-		enable = true,
-		extended_mode = true,
-		max_file_lines = nil,
-	}
-}
