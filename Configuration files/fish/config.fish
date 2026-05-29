5eed2f5c4baddb474fd6be90275639300dfc6ccf
Matus Mastena <Shadiness9530@proton.me>
2026-05-29 07:46:56 +0200

    chore: Remove unnecesary things

--- Configuration files/fish/config.fish
+++ Configuration files/fish/config.fish
@@ -9,11 +9,15 @@ starship init fish | source
 alias cat="bat --paging=never"
 alias ls="eza -lhi --git --icons"
 
-
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 set --export PAGER "$(command -v nvim) +Man!"
+set --export DOTNET_ROOT "/home/linuxbrew/.linuxbrew/opt/dotnet/libexec"
 export PATH="$HOME/.local/bin:$PATH"
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
+
+
+# Added by Antigravity CLI installer
+set -gx PATH "/home/matus/.local/bin" $PATH
